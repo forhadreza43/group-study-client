c6b574407c2e1d914ea6515a169eab6e7a52501f
Forhad Reza <forhad.bimt@email.com>
2025-06-10 10:36:19 +0600

    Initilize Project

---
+++ src/context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,8 @@
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const authInfo = {};
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
