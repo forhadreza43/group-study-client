81a0d32a1c672880b4feb53544be98eda5206eca
Forhad Reza <forhad.bimt@email.com>
2025-06-13 14:01:24 +0600

    Added smoothness to darkmode transaction

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="transition-colors duration-500">
       <Navbar />
       <div className="dark:bg-gray-800 pt-[68px]">
         <div className="w-11/12 mx-auto max-w-7xl min-h-[calc(100vh-68px)]">
